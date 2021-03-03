f4460e0b30a8deac2b8e7052fcdaf14edd204b76
Dane Murphy <danem7@gmail.com>
2021-03-03 15:48:34 -0800

    added change to route

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get('/api/test', (req, res) => {
     res.json({message: 'You have hit this end point!'});
 });
 
+app.get('/api/added', (req, res) => {
+    res.json({ mesage: 'Added route has been deployed by GitHub actions!' });
+})
+
 app.listen(5000, () => console.log('Server started on port 5000'));
